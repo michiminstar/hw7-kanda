e5bc684d4b19fcc26e909130082b523688df7e05
Michi Kanda <michi.kanda.0513@gmail.com>
2014-07-25 17:15:07 +0900

    second commit

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
-# code U
 import urllib2
 from flask import request, Flask
 from google.appengine.api import urlfetch
 
 app = Flask(__name__)
 
-
-
+@app.route('/')
+def index():
+	return 'Type "convert?message=" to the url and add some words at the end.'
+	
+@app.route('/convert')
+def convert():
+	message = request.args.get('message')
+	return 'awesome ' + message + ' !'
 
 @app.route('/show')
 def show():
+	print 'Welcome'
 	url = "http://step-test-krispop.appspot.com/"
 	try:
 	  result = urlfetch.fetch(url)
 	  return result.content
 	except urllib2.URLError, e:
-	  handleError(e)	
+		handleError(e)
 
 #	message = request.args.get('message')
 #	url = request.url_root + "convert?message=" + message
@@ -25,9 +31,11 @@ def show():
 #	print(response)
 #	return response.decode()
 
-@app.route('/convert')
-def convert():
-	print "Hello!"
-	message = request.args.get('message')
-	return 'awesome ' + message + ' !'
+# @app.route('/getword')
+# def getword():
+# 	pos = request.args.get('message')
+# 	params = {
+# 		'noun1':'dog'
+# 	}
+# 	return pos+ message
 
